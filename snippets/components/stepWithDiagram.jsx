3f1545c1f8f1ed8bc9f10530afc79e63261bd087
Todd Kerpelman <4397978+ToddKerpelman@users.noreply.github.com>
2025-09-18 15:15:20 -0700

    Fixes and better docs

--- snippets/components/stepWithDiagram.jsx
+++ snippets/components/stepWithDiagram.jsx
@@ -2,18 +2,16 @@ import React from "react";
 
 export const StepWithDiagram = ({ step, asset, hideBorder, children }) => {
   return (
-    <div className="flex items-start max-w-5xl mb-12 flex-col md:flex-row">
-      <div
-        className={`
-          pr-8 pt-4 flex
-          ${!hideBorder ? "border-t border-gray-300" : ""}
-          ${step === 1 && !hideBorder ? "border-gray-900" : ""}
-        `}
-      >
-        {step && <span className="mr-8 font-bold text-lg">{step}</span>}
-        <div className="flex-1">{children}</div>
+    <div
+      className={`flex items-start max-w-5xl mb-4 flex-col md:flex-row           
+        ${!hideBorder ? "border-t border-gray-300" : ""}
+        ${step === 1 && !hideBorder ? "border-gray-300" : ""}`}
+    >
+      <div className={`pr-8 pt-4 flex`}>
+        {step && <span className="mr-4 font-bold text-lg">{step}</span>}
+        <div className="flex-1 text-md">{children}</div>
       </div>
-      <div className="w-80 pl-0 md:pl-24 flex-shrink-0 mt-16 md:mt-0">
+      <div className="w-60 pl-0 md:pl-4 flex-shrink-0 mt-16 md:mt-0">
         <img
           className="w-full h-auto"
           src={asset}
